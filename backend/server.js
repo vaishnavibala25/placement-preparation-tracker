7ba97151c7df13524a71957d0ba3003d94e408c6
Vaishnavi <vaishnavibalakrishnan2006@gmail.com>
2025-12-16 14:19:47 +0530

    Backend setup and MongoDB Connection

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Placement Preparation Tracker API is running");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
