485b22f60f9d9295a49bf59dd99f0bd6c53b8c39
Vaishnavi <vaishnavibalakrishnan2006@gmail.com>
2025-12-16 15:22:51 +0530

    User Authentication with JWT

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-
+dotenv.config();
 const app = express();
+connectDB();
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Placement Preparation Tracker API is running");
 });
+app.use("/api/auth", require("./routes/authRoutes"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
